ef39e88c6b7bcdc1ecc5162e22d46ba772b6965d
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-07-26 18:43:51 -0300

    add instance

--- painel_personalizadov2.php
+++ painel_personalizadov2.php
@@ -10,6 +10,14 @@ License: GLPV2
 Text Domain: primeiro plugin
 */
 
+class Segundo_plugin{
+    private static $instance;
+    public static function getInstance(){
+        if(self::$instance == NULL){
+            self::$instance = new self();
+        }
+    }
+}
 // desativar a ction welcome_panel
 remove_action('welcome_panel', 'wp_welcome_panel');
 add_action('welcome_panel','my_welcome_panel');
@@ -20,4 +28,8 @@ function my_welcome_panel(){
         <h3>Seja bem vindo Ao mundo de Bob</h3>
     </div>
 <?php 
+}
+function add_css(){
+    wp_register_style('meu-segundo-plugin',plugin_dir_url(__FILE__).'css/meu-segundo-plugin.css');
+    wp_enqueue_style('meu-segundo-plugin');
 }
